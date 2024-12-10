65aff14f361081c0f5665725930566cc26ba1a39
Y0urLocalCatboy <156523842+Y0urLocalCatboy@users.noreply.github.com>
2024-12-10 10:01:36 +0100

    Commit

--- HeartDiseaseAnalysis.py
+++ HeartDiseaseAnalysis.py
@@ -1,4 +1,21 @@
 import pandas as pd
 heart_disease_data = pd.read_csv('heart_disease_dataset.csv')
-print(heart_disease_data.head())
-print(heart_disease_data.describe())
+
+male_data = heart_disease_data[heart_disease_data["Sex"] == "male"]
+female_data = heart_disease_data[heart_disease_data["Sex"] == "female"]
+
+male_diseased_data = male_data[male_data["Disease"] == True]
+female_diseased_data = female_data[female_data["Disease"] == True]
+
+male_healthy_data = male_data[male_data["Disease"] == False]
+female_healthy_data = female_data[female_data["Disease"] == False]
+#print(male_healthy_data.head())
+if male_diseased_data.count()[1] > female_diseased_data.count()[1]:
+    print("More man than women have heart disease")
+elif male_diseased_data.count()[1] == female_diseased_data.count()[1]:
+    print("They are equal in their heart disease")
+else:
+    print("More woman than men have heart disease")
+
+
+print(male_healthy_data.sort_values(by="Serum cholesterol in mg/dl", ascending=False).describe())
